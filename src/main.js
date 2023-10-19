10f1fa5e2a1b5f8d78ff753df6ae2b0d3c266705
Gaby <gabriela.garcia2024@hilasal.superate.org.sv>
2023-10-18 21:34:00 -0600

    Puse la foto de los productos un medio banner y también las funciones de agregar al carrito, y la confirmacion de datos y de pago

--- src/main.js
+++ src/main.js
@@ -1,47 +1,44 @@
- // Clase Producto
- class Producto {
-    constructor(nombre, descripcion, precio) {
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.precio = precio;
-    }
+ // Esta función añade un producto al carrito de compras
+ function addToCart(name, price) {
+    // Crear un elemento li con el nombre y el precio del producto
+    let li = document.createElement("li");
+    li.textContent = name + " - $" + price;
+
+    // Añadir el elemento li al elemento ul con id "items"
+    let items = document.getElementById("items");
+    items.appendChild(li);
+
+    // Actualizar el elemento p con id "total" con el nuevo total
+    let total = document.getElementById("total");
+    let currentTotal = Number(total.textContent.split("$")[1]);
+    let newTotal = currentTotal + price;
+    total.textContent = "Total: $" + newTotal;
 }
 
-// Clase CarritoDeCompra
-class CarritoDeCompra {
-    constructor() {
-        this.productos = [];
-    }
-
-    agregarProducto(producto) {
-        this.productos.push(producto);
-    }
-
-    calcularTotal() {
-        return this.productos.reduce((total, producto) => total + producto.precio, 0);
-    }
+// Esta función muestra el formulario de compra al hacer clic en el botón de hacer compra
+function showForm() {
+    // Obtener el elemento form con id "buy-form"
+    let form = document.getElementById("buy-form");
+    // Cambiar la clase del elemento form a "flex flex-col gap-2 mt-4" para mostrarlo
+    form.className = "flex flex-col gap-2 mt-4";
 }
 
-// Evento para agregar productos al carrito
-const botonesAgregarAlCarrito = document.querySelectorAll('.bg-yellow-300');
-const carrito = new CarritoDeCompra();
+// Esta función valida los datos del formulario y muestra un mensaje al hacer clic en el botón de confirmar compra
+function confirmPurchase() {
+    // Obtener los valores de los elementos input del formulario
+    let cardNumber = document.getElementById("card-number").value;
+    let cardPassword = document.getElementById("card-password").value;
+    let name = document.getElementById("name").value;
+    let surname = document.getElementById("surname").value;
+    let address = document.getElementById("address").value;
 
-botonesAgregarAlCarrito.forEach((boton, index) => {
-    boton.addEventListener('click', () => {
-        const producto = new Producto(`Producto ${index + 1}`, `Descripción del Producto ${index + 1}`, 10.0);
-        carrito.agregarProducto(producto);
-
-        // Actualizar la lista del carrito
-        const listaCarrito = document.getElementById('carrito');
-        const li = document.createElement('li');
-        li.textContent = `${producto.nombre} - $${producto.precio}`;
-        listaCarrito.appendChild(li);
-    });
-});
-
-// Evento para realizar una orden de compra
-const realizarOrdenButton = document.getElementById('realizarOrden');
-realizarOrdenButton.addEventListener('click', () => {
-    const total = carrito.calcularTotal();
-    alert(`Orden de compra realizada. Total: $${total}`);
-});
+    // Verificar que los valores sean válidos
+    if (cardNumber == 12345 && cardPassword == 1234 && name && surname && address) {
+        // Mostrar un mensaje de éxito con el nombre y el total de la compra
+        let total = document.getElementById("total").textContent;
+        alert(`Gracias por tu compra, ${name}. Tu total es ${total}.`);
+    } else {
+        // Mostrar un mensaje de error con los datos inválidos
+        alert(`Los datos que ingresaste son inválidos. Por favor, verifica tu número de tarjeta, tu contraseña y tus datos personales.`);
+    }
+}
